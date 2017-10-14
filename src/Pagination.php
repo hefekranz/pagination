e4aa07bba04e147d92481ff6c8d276a410f7db1e
lm <l.mauritz@delinero.de>
2017-10-14 10:28:30 +0200

    Nodes and Parameters as Constants

--- src/Pagination.php
+++ src/Pagination.php
@@ -10,19 +10,31 @@ use Symfony\Component\HttpFoundation\Request;
 
 class Pagination
 {
-    /** @var int  */
+
+    const PARAMETER_PAGE = 'page';
+    const PARAMETER_LIMIT = 'limit';
+
+    const NODE_FIRST = 'first';
+    const NODE_PREVIOUS = 'previous';
+    const NODE_CURRENT = 'current';
+    const NODE_NEXT = 'next';
+    const NODE_LAST = 'last';
+    const NODE_PAGES = 'pages';
+    const NODE_LINKS = 'links';
+
+    /** @var int */
     private $limit;
 
     /** @var  int */
     private $totalCount;
 
-    /** @var int  */
+    /** @var int */
     private $first;
 
-    /** @var int  */
+    /** @var int */
     private $previous;
 
-    /** @var int  */
+    /** @var int */
     private $current;
 
     /** @var  int */
@@ -46,7 +58,7 @@ class Pagination
     /** @var  string */
     private $lastPage;
 
-    /** @var Request  */
+    /** @var Request */
     private $request;
 
 
@@ -84,7 +96,8 @@ class Pagination
         return $this;
     }
 
-    public function setLimit(int $limit) {
+    public function setLimit(int $limit)
+    {
         $this->limit = $limit;
         return $this;
     }
@@ -106,7 +119,7 @@ class Pagination
     public function build()
     {
 
-        if($this->totalCount == 0 || $this->limit > $this->totalCount) {
+        if ($this->totalCount == 0 || $this->limit > $this->totalCount) {
             $this->first = 1;
             $this->previous = 1;
             $this->current = 1;
@@ -134,52 +147,51 @@ class Pagination
     {
         $this->firstPage = $this->buildUrl(
             array(
-                'page' => $this->first,
-                'limit' => $this->getLimit()
+                self::PARAMETER_PAGE => $this->first,
+                self::PARAMETER_LIMIT => $this->getLimit()
             ));
         $this->previousPage = $this->buildUrl(
             array(
-                'page' => $this->previous,
-                'limit' => $this->getLimit()
+                self::PARAMETER_PAGE => $this->previous,
+                self::PARAMETER_LIMIT => $this->getLimit()
             ));
         $this->currentPage = $this->buildUrl(array(
-            'page' => $this->current,
-            'limit' => $this->getLimit()
+            self::PARAMETER_PAGE => $this->current,
+            self::PARAMETER_LIMIT => $this->getLimit()
         ));
         $this->nextPage = $this->buildUrl(array(
-            'page' => $this->next,
-            'limit' => $this->getLimit()
+            self::PARAMETER_PAGE => $this->next,
+            self::PARAMETER_LIMIT => $this->getLimit()
         ));
         $this->lastPage = $this->buildUrl(array(
-            'page' => $this->last,
-            'limit' => $this->getLimit()
+            self::PARAMETER_PAGE => $this->last,
+            self::PARAMETER_LIMIT => $this->getLimit()
         ));
 
     }
 
     private function calculateLimit()
     {
-        if($this->request) {
-                $this->limit = (int) $this->request->query->get('limit',$this->limit);
-            }
+        if ($this->request) {
+            $this->limit = (int)$this->request->query->get(self::PARAMETER_LIMIT, $this->limit);
+        }
     }
 
     private function calculateLast()
     {
-        $this->last = (int) ceil($this->totalCount / $this->limit);
+        $this->last = (int)ceil($this->totalCount / $this->limit);
     }
 
     private function calculateCurrent()
     {
-        if($this->request) {
-            $this->current = (int) $this->request->query->get('page',1);
+        if ($this->request) {
+            $this->current = (int)$this->request->query->get(self::PARAMETER_PAGE, 1);
         }
     }
 
     private function calculateNext()
     {
-        if($this->current == $this->last)
-        {
+        if ($this->current == $this->last) {
             $this->next = $this->last;
             return;
         }
@@ -189,7 +201,7 @@ class Pagination
 
     private function calculatePrevious()
     {
-        if($this->current == 1) {
+        if ($this->current == 1) {
             $this->previous = 1;
             return;
         }
@@ -201,7 +213,7 @@ class Pagination
      */
     public function getLimit()
     {
-        return (int) $this->limit;
+        return (int)$this->limit;
     }
 
     /**
@@ -209,7 +221,7 @@ class Pagination
      */
     public function getTotalCount()
     {
-        return (int) $this->totalCount;
+        return (int)$this->totalCount;
     }
 
     /**
@@ -217,7 +229,7 @@ class Pagination
      */
     public function getFirst()
     {
-        return (int) $this->first;
+        return (int)$this->first;
     }
 
     /**
@@ -225,7 +237,7 @@ class Pagination
      */
     public function getCurrent()
     {
-        return (int) $this->current;
+        return (int)$this->current;
     }
 
     /**
@@ -233,7 +245,7 @@ class Pagination
      */
     public function getPrevious()
     {
-        return (int) $this->previous;
+        return (int)$this->previous;
     }
 
     /**
@@ -241,7 +253,7 @@ class Pagination
      */
     public function getNext()
     {
-        return (int) $this->next;
+        return (int)$this->next;
     }
 
     /**
@@ -249,7 +261,7 @@ class Pagination
      */
     public function getLast()
     {
-        return (int) $this->last;
+        return (int)$this->last;
     }
 
     /**
@@ -295,7 +307,7 @@ class Pagination
 
     private function buildUrl($parameters)
     {
-        if(!$this->request) {
+        if (!$this->request) {
             return null;
         }
 
@@ -307,7 +319,7 @@ class Pagination
         }
         $query = http_build_query($currentQueryParams);
 
-        return $currentPath.'?'.$query;
+        return $currentPath . '?' . $query;
     }
 
     /**
@@ -324,23 +336,23 @@ class Pagination
     public function __toArray()
     {
         $links = array(
-            'first' => $this->firstPage,
-            'previous' => $this->previousPage,
-            'current' => $this->currentPage,
-            'next' => $this->nextPage,
-            'last' => $this->lastPage
+            self::NODE_FIRST => $this->firstPage,
+            self::NODE_PREVIOUS => $this->previousPage,
+            self::NODE_CURRENT => $this->currentPage,
+            self::NODE_NEXT => $this->nextPage,
+            self::NODE_LAST => $this->lastPage
         );
         $pages = array(
-            'first' => $this->first,
-            'previous' => $this->previous,
-            'current' => $this->current,
-            'next' => $this->next,
-            'last' => $this->last
+            self::NODE_FIRST => $this->first,
+            self::NODE_PREVIOUS => $this->previous,
+            self::NODE_CURRENT => $this->current,
+            self::NODE_NEXT => $this->next,
+            self::NODE_LAST => $this->last
         );
 
         return $data = array(
-            'pages' => $pages,
-            'links' => $links
+            self::NODE_PAGES => $pages,
+            self::NODE_LINKS => $links
         );
 
     }
